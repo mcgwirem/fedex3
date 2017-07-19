878eac4ab8ce57e724e9f02ecfe041b089bd7252
mcgwirem <mmcgwire67@gmail.com>
2017-07-19 16:36:05 -0700

    23

--- test.php
+++ test.php
@@ -37,6 +37,10 @@ echo $path_to_wsdl;
 
 $client = new SoapClient($path_to_wsdl/*, array('trace' => 1)*/); // Refer to http://us3.php.net/manual/en/ref.soap.php for more information
 
+if (is_soap_fault($client)) {
+	echo '<p>error in soap</p>'
+}
+
 echo $client;
 
 echo '<p> after $client </p>';
