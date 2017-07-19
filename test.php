acf4ffe6fdc474222d94a68ba0f9a08917a2c80a
mcgwirem <mmcgwire67@gmail.com>
2017-07-19 16:22:00 -0700

    22

--- test.php
+++ test.php
@@ -35,7 +35,7 @@ echo '<p> after init </p>';
 
 echo $path_to_wsdl;
 
-$client = new SoapClient($path_to_wsdl, array('trace' => 1)); // Refer to http://us3.php.net/manual/en/ref.soap.php for more information
+$client = new SoapClient($path_to_wsdl/*, array('trace' => 1)*/); // Refer to http://us3.php.net/manual/en/ref.soap.php for more information
 
 echo $client;
 
