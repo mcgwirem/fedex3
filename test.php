e64dc5820776f6c622dee902ef594f9f0917b625
mcgwirem <mmcgwire67@gmail.com>
2017-07-24 14:26:00 -0700

    32

--- test.php
+++ test.php
@@ -37,7 +37,7 @@ echo '<p> after init </p>';
 
 echo $path_to_wsdl;
 
-//$client = new SoapClient($path_to_wsdl, array('trace' => 1, "AccountNumer" = 1234, )); // Refer to http://us3.php.net/manual/en/ref.soap.php for more information
+$client = new SoapClient($path_to_wsdl, array('trace' => 1)); // Refer to http://us3.php.net/manual/en/ref.soap.php for more information
 
 if (is_soap_fault($client)) {
 	echo '<p>error in soap</p>';
