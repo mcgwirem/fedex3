e83bc4a4fca5e4669e3df912e53b4c2eafe47555
mcgwirem <mmcgwire67@gmail.com>
2017-07-24 15:25:38 -0700

    43

--- FedEx_PHP.php
+++ FedEx_PHP.php
@@ -77,7 +77,7 @@ print '<pre>';
 print 'Rate: $';
 //print (string) $result->SOAPENVBody->RateReply->RateReplyDetails->RatedShipmentDetails[0]->ShipmentRateDetail->TotalNetCharge->Amount;
 //print (string) $result -> SOAPENVBody -> TrackReply -> CompletedTrackDetails -> TrackDetails;
-print (string) $result -> SOAPENVBody -> TrackReply -> CompletedTrackDetails -> TrackDetails -> StatusDetail;
+print (string) $result -> SOAPENVBody -> TrackReply -> CompletedTrackDetails;
 print '<hr/>';
 print_r($result);
 ?>
