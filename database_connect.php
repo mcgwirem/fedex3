bee973b1f08baaf1d92c243938f5299e11585707
mcgwirem <mmcgwire67@gmail.com>
2017-07-28 08:39:20 -0700

    Delete database_connect.php

--- database_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhost = 'sqlsrv:server=tcp:travismathew-analytics-database1.database.windows.net,1433;Database=testDB';
-$dbname = 'testDB';
-$dbuser = 'null@travismathew-analytics-database1.database.windows.net';
-$dbpswd = 'Polos2016';
-$dbuserserver = 'mmcgwire@@travismathew-analytics-database1';
-?>
