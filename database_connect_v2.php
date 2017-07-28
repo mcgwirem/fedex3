b15e77d3e55b0e4abf283c382a5095af8dc22548
mcgwirem <mmcgwire67@gmail.com>
2017-07-28 08:39:37 -0700

    Update database_connect_v2.php

--- database_connect_v2.php
+++ database_connect_v2.php
@@ -1,12 +1,4 @@
 <?php
-define('db_user', 'mmcgwire');
-define('db_pswd', '5WOPLjwie$6QZI^B04fw19MafyTcd#ozVhW*');
-define('db_host', 'travismathew-analytics-database1.database.windows.net');
-define('db_name', 'testDB');
-define('fe_key', 'LH9RgUjrvEqHHgaq');
-define('fe_pswd', '3vo6fcxMhfN3ejYRBZPiESJTM');
-define('fe_acct', '304955554');
-define('fe_meter', '111359514');
 
 //include(functions.php);
 
